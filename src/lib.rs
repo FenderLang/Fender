fd5ed1802def06a46e3c09c7c3466ace5e214ec0
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2023-03-23 17:25:52 -0400

    windows update

--- src/lib.rs
+++ src/lib.rs
@@ -14,34 +14,4 @@ pub mod stdlib;
 #[cfg(test)]
 mod test;
 
-#[macro_export]
-macro_rules! count {
-    ($first:pat_param, $($rest:pat_param),*) => {
-        1 + $crate::count!($($rest),*)
-    };
-    ($first:pat_param) => {1};
-    () => {0};
-}
 
-#[macro_export]
-macro_rules! fndr_native_func {
-    (
-        $(#[$docs:meta])*
-        $name:ident, | $ctx:tt $(, $($arg:pat_param),*)? | $body:expr
-    ) => {
-        $(#[$docs])*
-        #[allow(unused)]
-        pub fn $name(
-            $ctx: &mut freight_vm::execution_engine::ExecutionEngine<
-                $crate::type_sys::type_system::FenderTypeSystem
-            >,
-            args: Vec<$crate::fender_reference::FenderReference>,
-        ) -> Result<$crate::fender_reference::FenderReference, freight_vm::error::FreightError> {
-            const _ARG_COUNT: usize = $crate::count!($($($arg),*)?);
-            $(
-                    let [$($arg),*]: [$crate::fender_reference::FenderReference; _ARG_COUNT]  = args.try_into().unwrap();
-                    )?
-            $body
-        }
-    }
-}
