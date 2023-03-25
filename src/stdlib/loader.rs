86209bfd1c4f7c1d9472a3fa9f724b6a588ea7a7
Redempt <instantiable@gmail.com>
2023-03-24 21:32:29 -0400

    Redefine stdlib with new macro

--- src/stdlib/loader.rs
+++ src/stdlib/loader.rs
@@ -4,8 +4,18 @@ use freight_vm::{function::FunctionWriter, vm_writer::VMWriter};
 use crate::type_sys::type_system::FenderTypeSystem;
 
 #[macro_export]
+macro_rules! dep_name {
+    ($name:literal => $variant_name:ident) => {
+        $name
+    };
+    ($variant_name:ident) => {
+        stringify!($variant_name)
+    };
+}
+
+#[macro_export]
 macro_rules! deps_enum {
-    ($name:ident, $count:ident : $($val:ident),*) => {
+    ($name:ident, $count:ident : $($(@ $string_name:literal)? $val:ident => $res:expr),* $(,)?) => {
         /// Number of valid dependencies
         pub const $count: usize = $crate::count!($($val),*);
         #[allow(non_camel_case_types, missing_docs)]
@@ -15,13 +25,21 @@ macro_rules! deps_enum {
         impl $name {
             fn from_str(s: &str) -> Option<$name> {
                 match s {
-                    $(stringify!($val) => Some($name::$val)),*,
+                    $(dep_name!($($string_name =>)? $val) => Some($name::$val)),*,
                     _ => None
                 }
             }
 
             fn all() -> impl IntoIterator<Item = $name> {
                 [$($name::$val),*]
+            }
+
+            fn load(&self, vm: &mut freight_vm::vm_writer::VMWriter<FenderTypeSystem>, func: &mut freight_vm::function::FunctionWriter<FenderTypeSystem>) -> usize {
+                match self {
+                    $(
+                        $name::$val => $res.load_into(vm, func)
+                    ),*
+                }
             }
         }
         impl Into<usize> for $name {
