ebc8e6a32ee2ed619906521b61e5ce23227bbb64
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2023-03-30 14:11:20 -0400

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use fender::interpreter;
-use std::{fs, path::Path, process::exit};
+use std::{fs, io::BufRead, path::Path, process::exit};
 
 fn main() {
-    let file = std::env::args().nth(1).unwrap();
-    let file = Path::new(&file);
-    let script = fs::read_to_string(file).unwrap();
+    let args = std::env::args().collect::<Vec<_>>();
+    let script = if args.len() < 2 {
+        let mut script = String::new();
+        let stdin = std::io::stdin();
+        for line in stdin.lock().lines() {
+            match line {
+                Ok(l) => script.push_str(&l),
+                Err(_) => break,
+            };
+            script.push('\n');
+        }
+        script
+    } else {
+        if let Ok(true) = Path::new(&args[1]).try_exists() {
+            fs::read_to_string(&args[1]).unwrap()
+        } else {
+            args[1].clone()
+        }
+    };
+
     let mut vm = match interpreter::create_vm(&script) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("Error proccessing fender code: {}", e);
+            eprintln!("Error processing fender code: {}", e);
             exit(1);
         }
     };
