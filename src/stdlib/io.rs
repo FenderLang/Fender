4a2b49ae884a6b4a045f827e627d351cb8d3692b
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2023-04-14 12:48:53 -0400

    error prints

--- src/stdlib/io.rs
+++ src/stdlib/io.rs
@@ -41,6 +41,42 @@ fndr_native_func!(
     }
 );
 
+
+fndr_native_func!(
+    /// Prints to stderr
+    eprint_func,
+    |_, item, argv| {
+        eprint!("{}", item.to_string());
+        let argv = match &*argv {
+            List(l) => l.deref(),
+            e => unreachable!("Last argument is always a vararg list, found: {:?}", e),
+        };
+        for item in argv {
+            eprint!("{}", item.to_string());
+        }
+        Ok(item)
+    }
+);
+
+fndr_native_func!(
+    /// Prints to stderr and adds a newline
+    eprintln_func,
+    |_, item, argv| {
+        eprint!("{}", item.to_string());
+        let argv = match &*argv {
+            List(l) => l.deref(),
+
+            e => unreachable!("Last argument is always a vararg list, found: {:?}", e),
+        };
+        for item in argv {
+            eprint!("{}", item.to_string());
+        }
+        eprintln!();
+
+        Ok(item)
+    }
+);
+
 // --- stdin ---
 
 fndr_native_func!(
--- src/stdlib/mod.rs
+++ src/stdlib/mod.rs
@@ -102,6 +102,8 @@ impl StdlibResource for FenderNativeTypeValue {
 deps_enum! {FenderResource, STDLIB_SIZE:
         print => FenderNativeFunction {func: io::print_func, args: variadic(1..)},
         println => FenderNativeFunction {func: io::println_func, args: variadic(1..)},
+        eprint => FenderNativeFunction {func: io::eprint_func, args: variadic(1..)},
+        eprintln => FenderNativeFunction {func: io::eprintln_func, args: variadic(1..)},
         readLine => FenderNativeFunction {func: io::read_line_func, args: fixed(0)},
         read => FenderNativeFunction {func: io::read_func, args: fixed(1)},
         write => FenderNativeFunction {func: io::write_func, args: fixed(2)},
